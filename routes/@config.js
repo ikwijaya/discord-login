506b514c87e80b83e30d1def7dd94af784d3951a
ikwijaya <irpan.kusuma@gmail.com>
2022-07-23 00:39:50 +0700

    abi from be

--- routes/@config.js
+++ routes/@config.js
@@ -1,12 +1,14 @@
 
 const router = require('express').Router()
 const { utils } = require('../utils')
+const abi = require('../assets/abi.json')
 const { TOKEN_ADDRESS } = process.env
 
 router.get('/', async (req, res, next) => {
   try {
     res.send(utils.success(`load success`, {
-      token_address: TOKEN_ADDRESS
+      token_address: TOKEN_ADDRESS,
+      abi: abi
     }))
   } catch (err) {
     res.status(401).send(utils.failed(err, []))
